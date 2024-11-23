5305bbbf1020046b5f85247fcb50e39291aa7f4c
TDung939 <dungnguyenhoangtrung@gmail.com>
2024-11-23 16:28:57 +0700

    New commit

--- game.js
+++ game.js
@@ -4,7 +4,8 @@ let speed = 3;
 const objects = [
   // { el: document.getElementById('oe'), lastX: 60, targetX: 0, speed: 6},
   { el: document.getElementById('bong'), lastX: 940, targetX: 0, speed: 4},
-  { el: document.getElementById('hai'), lastX: 640, targetX: 0, speed: 2}
+  { el: document.getElementById('hai'), lastX: 640, targetX: 0, speed: 2},
+  { el: document.getElementById('bep'), lastX: 230, targetX: 0, speed: 3}
 ];
 
 function moveObject(obj) {
